729551331dc89b0a7d2b145892ab972970352178
Maestro Meus <kharizimo@gmail.com>
2026-03-13 15:30:36 +0100

    updates

--- pages/view.php
+++ pages/view.php
@@ -1,4 +1,11 @@
 <?php
+// ajax
+if($_a=='update-periode'){
+    $sql="update demande set date_debut='$date_debut',date_fin='$date_fin' where id=$id";
+    $cn->exec($sql);
+    header("location:view?id=$id");
+    exit;
+}
 $s??='form';
 $sql="select * from v_demande where id=$id";
 $demande=$cn->query($sql)->fetch();
